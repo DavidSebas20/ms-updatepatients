f28a1e0cfd3b20b8575610820d6e6b7b945b2076
DavidSebas20 <davidsebastian20@outlook.com>
2025-01-22 18:55:50 -0500

    :tada: initialize updatepatients Spring Boot application with Maven setup and basic configuration

---
+++ src/main/java/com/example/updatepatients/UpdatepatientsApplication.java
@@ -0,0 +1,13 @@
+package com.example.updatepatients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdatepatientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdatepatientsApplication.class, args);
+	}
+
+}
